5aba53429eb8a62b018d868404f33ca6d295a45b
Francisco <juniorfchj2@gmail.com>
2019-10-11 17:21:23 -0300

    Graphics

---
+++ src/pages/Teste/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import Header from '../../componentes/Header'
+import Menu from '../../componentes/Menu'
+
+import axios from 'axios';
+
+
+function TempTotem({id, color, name}){
+    return <div style={{color: color}}>{name} id: {id}
+        <Link to={`/totem/${id}`}>Mais detalhes</Link>
+    </div>;
+}
+
+// Exemplo com outros gráficos: 
+// https://github.com/jerairrest/react-chartjs-2
+export default function Teste(){
+    const [lines, setLines] = React.useState([]);
+
+    React.useEffect(() => {
+        async function getLines(){
+            const res = await axios.get('http://localhost:4550/lines/list');
+            setLines(res.data.data);
+            console.log(res.data);
+        }
+        getLines();
+        
+    }, []);
+
+    return <>
+        <Header />
+        <Menu />
+        <h2>teste</h2> 
+        {lines.map(line => <TempTotem color={line.color} name={line.name} id={line.id}/>)}
+    </>
+}
