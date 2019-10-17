7b81d406774ea682b07af12a5ff9fbb5377641e4
fernandacaramico <fernandacaramico@gmail.com>
2019-10-17 20:32:43 -0300

    adiciona pagina totem-info-fim

--- src/pages/TotemInfo/index.jsx
+++ src/pages/TotemInfo/index.jsx
@@ -8,11 +8,21 @@ import CurrentStatus from '../../componentes/CurrentStatus'
 
 // Exemplo com outros gráficos: 
 // https://github.com/jerairrest/react-chartjs-2
-export default function Home(){
+export default function Home() {
     return <>
         <Header />
         <Menu />
-        <CurrentStatus/>
-
+        <div class="containerTotem">
+            <CurrentStatus />
+            <div className="item">
+            <img src="https://aws1.discourse-cdn.com/elastic/original/2X/5/5142ad613f9ce060017b1cc2c8ca0a8dfccbc4fe.png" alt="grafico 1"/>
+            </div>
+            <div className="item">
+            <img src="https://aws1.discourse-cdn.com/elastic/original/2X/5/5142ad613f9ce060017b1cc2c8ca0a8dfccbc4fe.png" alt="grafico 2"/>
+            </div>
+            <div className="item">
+            <img src="https://aws1.discourse-cdn.com/elastic/original/2X/5/5142ad613f9ce060017b1cc2c8ca0a8dfccbc4fe.png" alt="grafico 3"/>
+            </div>
+        </div>
     </>
 }
